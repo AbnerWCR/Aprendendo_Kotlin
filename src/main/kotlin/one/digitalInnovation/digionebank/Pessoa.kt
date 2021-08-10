cce299977f1d8ff9ad1e6090ad55fff53591734a
AbnerWCR <abnerwcrodrigues@gmail.com>
2021-08-10 14:11:51 -0300

    acrescentando membros de uma classe

--- src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
@@ -2,15 +2,20 @@ package one.digitalInnovation.digionebank
 
 class Pessoa {
     var nome: String? = null
-    var cpf: String? = null
+    var cpf: String? = "123.456.782-88"
         private set
+
+    constructor()
+
+    fun dadosPessoa() = "nome: $nome e cpf: $cpf"
 }
 
 fun main() {
     val pessoa = Pessoa()
 
-    pessoa.nome = "Joao"
-    println(pessoa.nome)
-    println(pessoa.cpf)
+    var sobrenome: String? = "Silva"
+
+    pessoa.nome = "Joao $sobrenome"
+    println(pessoa.dadosPessoa())
 
 }
