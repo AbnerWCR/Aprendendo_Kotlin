8ba6a69a518bea69a67addf990b454a12807b425
AbnerWCR <abnerwcrodrigues@gmail.com>
2021-08-14 14:58:18 -0300

    TesteClienteTipo criado e executado - finalizando aula Enum

--- src/main/kotlin/one/digitalInnovation/digionebank/testes/TesteClienteTipo.kt
+++ src/main/kotlin/one/digitalInnovation/digionebank/testes/TesteClienteTipo.kt
@@ -4,10 +4,13 @@ import one.digitalInnovation.digionebank.ClienteTipo
 
 fun main() {
     ClienteTipo.values().forEach { elemento ->
-        println("${elemento.name} -- $elemento")
+        println("${elemento.name} -- ${elemento.descricao}")
     }
 
     ClienteTipo.values().forEach {
-        println("${it.name} -- $it")
+        println("${it.name} -- ${it.descricao}")
     }
+
+    val pj = ClienteTipo.PJ
+    println("${pj.name} -- ${pj.descricao}")
 }
